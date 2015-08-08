82ac14a3a4063d0d165ed43d48c5f1b615b5a85e
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-08-08 12:55:19 +0200

    Update url-base64.js tests

--- test/url-base64.js
+++ test/url-base64.js
@@ -5,7 +5,9 @@ describe("url-base64", function() {
     describe("functions", function() {
         describe("#escape()", function() {
             it("should escape URL characters within Base64 string", function() {
-                assert.equal(URLBase64.escape("RXNjYXBlL1VuZXNjYXBlIFVSTCBjaGFyYWN0ZXJzIHdpdGhpbiBCYXNlNjQgc3RyaW5nLg=="), "RXNjYXBlL1VuZXNjYXBlIFVSTCBjaGFyYWN0ZXJzIHdpdGhpbiBCYXNlNjQgc3RyaW5nLg", "URL characters should be escaped within Base64 string");
+                var escapedBase64String = URLBase64.escape("RXNjYXBlL1VuZXNjYXBlIFVSTCBjaGFyYWN0ZXJzIHdpdGhpbiBCYXNlNjQgc3RyaW5nLg==");
+                var expectedEscapedBase64String = "RXNjYXBlL1VuZXNjYXBlIFVSTCBjaGFyYWN0ZXJzIHdpdGhpbiBCYXNlNjQgc3RyaW5nLg";
+                assert.equal(escapedBase64String, expectedEscapedBase64String, "URL characters should be escaped within Base64 string");
             });
 
             it("should not be overridden", function() {
@@ -21,7 +23,9 @@ describe("url-base64", function() {
 
         describe("#unescape()", function() {
             it("should unescape URL characters within Base64 string", function() {
-                assert.equal(URLBase64.unescape("RXNjYXBlL1VuZXNjYXBlIFVSTCBjaGFyYWN0ZXJzIHdpdGhpbiBCYXNlNjQgc3RyaW5nLg"), "RXNjYXBlL1VuZXNjYXBlIFVSTCBjaGFyYWN0ZXJzIHdpdGhpbiBCYXNlNjQgc3RyaW5nLg==", "URL characters should be unescaped within Base64 string");
+                var unescapedBase64String = URLBase64.unescape("RXNjYXBlL1VuZXNjYXBlIFVSTCBjaGFyYWN0ZXJzIHdpdGhpbiBCYXNlNjQgc3RyaW5nLg");
+                var expectedUnescapedBase64String = "RXNjYXBlL1VuZXNjYXBlIFVSTCBjaGFyYWN0ZXJzIHdpdGhpbiBCYXNlNjQgc3RyaW5nLg==";
+                assert.equal(unescapedBase64String, expectedUnescapedBase64String, "URL characters should be unescaped within Base64 string");
             });
 
             it("should not be overridden", function() {
